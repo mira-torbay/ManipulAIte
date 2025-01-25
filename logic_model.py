c393127c9daae0dd7348f1e5dd5013e76a8a276a
Mira Torbay <21met19@queensu.ca>
2025-01-25 13:33:50 -0500

    roberta-large-mnli

---
+++ logic_model.py
@@ -0,0 +1,18 @@
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+# Load the pipeline
+pipe = pipeline("text-classification", model="FacebookAI/roberta-large-mnli")
+
+# Load model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("FacebookAI/roberta-large-mnli")
+model = AutoModelForSequenceClassification.from_pretrained("FacebookAI/roberta-large-mnli")
+
+# Load zero-shot classification pipeline
+classifier = pipeline('zero-shot-classification', model='FacebookAI/roberta-large-mnli')
+
+sequence_to_classify = "one day I will see the world"
+candidate_labels = ['travel', 'cooking', 'dancing']
+result = classifier(sequence_to_classify, candidate_labels)
+
+print(result)
+
